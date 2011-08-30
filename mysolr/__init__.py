9f0e63a131d5aa5b354d4f2c6767e831c36be6a5
moliware <miguel@moliware.com>
2011-08-30 22:21:37 +0200

    Pypi-compatible setup.py

--- mysolr/__init__.py
+++ mysolr/__init__.py
@@ -1,16 +1,4 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-"""
-
-__author__ = "Rubén Abad and Miguel Olivares"
-__credits__ = ["Rubén Abad", "Miguel Olivares", "Francisco Javier Alba",
-               "Carlos Navarro"]
-__license__ = "GPL"
-__version__ = "0.0.1"
-__maintainer__ = "Rubén Abad"
-__email__ = "ruabag@gmail.com"
-__status__ = "Development"
 
 from mysolr_response import SolrResponse
 from mysolr import Solr
--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 
-import mysolr
 
-setup(name = 'mysolr',
-      version = '0.1',
-      description = 'Solr Python binding',
-      author = mysolr.__author__,
-      author_email = mysolr.__email__,
-      url = 'http://github.com/rabad/mysolr',
-      packages = ['mysolr']
-      )
+REQUIRED = ['requests>=0.6']
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+
+setup(name='mysolr',
+      version='0.1',
+      description='Solr Python binding',
+      author='Rubén Abad',
+      author_email='ruabag@gmail.com',
+      url='http://github.com/rabad/mysolr',
+      packages=['mysolr'],
+      install_requires=REQUIRED,
+      classifiers=CLASSIFIERS)
