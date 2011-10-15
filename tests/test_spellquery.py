10f2cdbc93dc696fd5f73a456db9fa6f9bd910c9
moliware <miguel@moliware.com>
2011-10-15 11:59:00 +0200

    Added test for spellchecking

---
+++ tests/test_spellquery.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+from mysolr import SolrResponse
+from os.path import join, dirname
+
+
+class SpellQueryTestCase(unittest.TestCase):
+    """ """
+
+    def setUp(self):
+        mock_file = join(dirname(__file__), 'mocks/spellquery')
+        mock = eval(open(mock_file).read())
+        self.response = SolrResponse(mock)
+
+    def tearDown(self):
+        pass
+
+    def test_query(self):
+        self.assertNotEqual(self.response.stats, None)
+
+
+if __name__ == '__main__':
+    unittest.main()
