fc13fb86f905cc7cf01e5d846b91106c6552f4bd
moliware <miguel@moliware.com>
2014-04-06 12:26:46 +0200

    Adds 3.4 as supported version

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python',
     'Topic :: Software Development :: Libraries :: Python Modules'
 ]
